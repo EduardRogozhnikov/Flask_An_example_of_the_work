02e414c060b3cccc3c2a3de264e93976d0c989ab
Edward <rogozhnikovv7@mail.ru>
2024-01-16 15:26:46 +0300

    new commit. Add create and posts file. New class in app.py, and new func.

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, redirect, request, Response, session
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_of_the_work.db'
+db = SQLAlchemy(app)
+
+
+class Arcticle(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow, autoincrement=True)
+
+    def __repr__(self):
+        return '<Arcticle %r>' % self.id
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@app.route('/posts')
+def posts():
+    posts_ = Arcticle.query.all()
+    return render_template('posts.html', posts=posts)
 
 
 @app.route('/email')
@@ -27,6 +47,25 @@ def mail_other():
 @app.route("/mail_new")
 def mail_new():
     return render_template("letter_example_3.html")
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        post_ = Arcticle(title=title, text=text)
+
+        try:
+            db.session.add(post_)
+            db.session.commit()
+            return redirect('/')
+
+        except:
+            return "При добавлении статьи произошла ошибка"
+
+    else:
+        return render_template('create.html')
 
 
 if __name__ == "__main__":
